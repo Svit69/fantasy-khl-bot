85cd26ed026fead5653e6048d3634326be599479
Svit69 <71544755+Svit69@users.noreply.github.com>
2025-07-27 22:09:05 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -147,8 +147,12 @@ async def main():
 
 if __name__ == '__main__':
     import nest_asyncio
+    import asyncio
+
     nest_asyncio.apply()
-    asyncio.run(main())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+
 
 
 
