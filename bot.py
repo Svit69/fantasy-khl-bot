665e934ebc52fc295755e9ed563a2480c992108f
Svit69 <71544755+Svit69@users.noreply.github.com>
2025-07-29 10:01:37 +0000

    Вернул запуск через asyncio.run(main()) для корректной работы в консоли

--- bot.py
+++ bot.py
@@ -100,8 +100,5 @@ async def main():
 
 if __name__ == '__main__':
     import asyncio
-    import nest_asyncio
-    nest_asyncio.apply()
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    asyncio.run(main())
 
