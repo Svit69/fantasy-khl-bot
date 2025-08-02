aed6d94c26d764dbe200a5dae62b0337ad731dc7
Svit69 <ol.svitenkov@gmail.com>
2025-08-02 13:34:23 +0500

    fix: исправлен SyntaxError в сообщении о повторной регистрации

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -42,8 +42,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     else:
         await message.reply_text(
             "⚠️ Ты уже в списке *генеральных менеджеров Фентези Драфта КХЛ*\.
-\n"
-            "*Формируй состав* и следи за результатами туров \- /tour",
+\n*Формируй состав* и следи за результатами туров \- /tour",
             reply_markup=markup,
             parse_mode="MarkdownV2"
         )
