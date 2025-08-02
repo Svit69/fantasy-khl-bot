d3a4513b41f84d7d4946ff91192f420ab032d9be
Svit69 <ol.svitenkov@gmail.com>
2025-08-02 13:36:21 +0500

    fix: сообщение о повторной регистрации  одна строка, без SyntaxError

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -41,11 +41,9 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         await message.reply_text(msg_id + msg, reply_markup=markup)
     else:
         await message.reply_text(
-            (
-                "⚠️ Ты уже в списке *генеральных менеджеров Фентези Драфта КХЛ*\.
-\n"
-                "*Формируй состав* и следи за результатами туров \- /tour"
-            ),
+            "⚠️ Ты уже в списке *генеральных менеджеров Фентези Драфта КХЛ*.\
+\n\
+*Формируй состав* и следи за результатами туров - /tour",
             reply_markup=markup,
             parse_mode="MarkdownV2"
         )
